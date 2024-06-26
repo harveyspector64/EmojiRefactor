27b8fb4dc5fe873205fc9cb9f469514526b79215
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-06-26 07:20:12 -0700

    Update tree.js

--- tree.js
+++ tree.js
@@ -3,6 +3,8 @@ import { EMOJIS } from './constants.js';
 export class Tree {
     constructor(x, y) {
         this.element = this.createTreeElement(x, y);
+        this.birds = [];
+        this.nest = null;
     }
 
     createTreeElement(x, y) {
@@ -16,7 +18,61 @@ export class Tree {
     }
 
     update() {
-        // Update tree state if needed
+        if (this.nest) {
+            this.nest.update();
+        }
+    }
+
+    addBird(bird) {
+        this.birds.push(bird);
+    }
+
+    removeBird(bird) {
+        const index = this.birds.indexOf(bird);
+        if (index > -1) {
+            this.birds.splice(index, 1);
+        }
+    }
+
+    createNest() {
+        if (!this.nest) {
+            this.nest = new Nest(this);
+            this.element.appendChild(this.nest.element);
+        }
+    }
+}
+
+class Nest {
+    constructor(tree) {
+        this.tree = tree;
+        this.element = this.createNestElement();
+        this.hatchTimer = null;
+        this.startHatchTimer();
+    }
+
+    createNestElement() {
+        const nestElement = document.createElement('div');
+        nestElement.textContent = '🥚';
+        nestElement.classList.add('emoji', 'nest');
+        nestElement.style.position = 'absolute';
+        nestElement.style.left = '0px';
+        nestElement.style.top = '-30px';
+        return nestElement;
+    }
+
+    startHatchTimer() {
+        const hatchTime = Math.random() * 60000 + 120000;
+        this.hatchTimer = setTimeout(() => this.hatch(), hatchTime);
+    }
+
+    update() {
+        // Nests don't need regular updates
+    }
+
+    hatch() {
+        this.element.remove();
+        this.tree.nest = null;
+        // Logic for adding new birds would go here
     }
 }
 
