7d08470c80c3eaa1bfa88e17aabb65d94422304c
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-06-25 21:29:22 -0700

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,13 @@
+// constants.js
+export const EMOJIS = {
+    BUSH: '🌺',
+    TREE: '🌳',
+    BUTTERFLY: '🦋',
+    BIRD: '🐦',
+    WORM: '🐛'
+};
+
+const INITIAL_EMOJIS = [
+    { id: 'flowering-bush', emoji: EMOJIS.BUSH },
+    { id: 'tree', emoji: EMOJIS.TREE, disabled: true }
+];
